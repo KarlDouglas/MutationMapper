871b80260c9b68372eff128855e3cdffcaa2fbb6
Karl <k.cahill98@gmail.com>
2022-11-10 13:10:40 +0100

    .

--- commands.py
+++ commands.py
@@ -1,3 +1,2 @@
 import main
-#masked = main.filter("fastq_demofile.txt")
 cut = main.sort_barcodes("/home/ptc872_ku_dk/modi_mount/unzip/X204SC22080130-Z01-F001/raw_data/CY002/CY002_FDDP220275354-1a_H7VYHDRX2_L1_1.fq")
