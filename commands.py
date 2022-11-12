2dc2d30b0eccfa72a287c7c9ea2561ea552e65fe
Karl <k.cahill98@gmail.com>
2022-11-12 11:05:20 +0100

    .

--- commands.py
+++ commands.py
@@ -1,2 +1,4 @@
 import main
-cut = main.sort_barcodes("/home/ptc872_ku_dk/modi_mount/unzip/X204SC22080130-Z01-F001/raw_data/CY002/CY002_FDDP220275354-1a_H7VYHDRX2_L1_1.fq")
+sort1 = main.sort_barcodes("/home/ptc872_ku_dk/modi_mount/unzip/X204SC22080130-Z01-F001/raw_data/CY002/CY002_FDDP220275354-1a_H7VYHDRX2_L1_1.fq")
+sort2 = main.sort_barcodes("/home/ptc872_ku_dk/modi_mount/unzip/X204SC22080130-Z01-F001/raw_data/CY002/CY002_FDDP220275354-1a_H7VYHDRX2_L1_1.fq")
+
