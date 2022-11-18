8703aea0c0d31e25cc8d43150ce7dc05207890ed
ptc872 <ptc872@alumni.ku.dk>
2022-11-18 15:20:58 +0100

    .

--- commands.py
+++ commands.py
@@ -1,7 +1,12 @@
 import main
 mask1 = main.mask("fastq_demofile.txt")
 mask2 = main.mask("fastq_demofile2.txt")
-paired = main.fastq_pair(mask1,mask2)
+paired = main.fastq_pair(mask1, mask2)
 sort = main.sort_barcodes(paired)
-merged = main.merge(sort)
+#merged = main.merge(sort)
+for i in sort:
+    main.merge2(i)
+#fasta_file = main.write_fasta(merged,"file1")
+
+
 
--- main.py
+++ main.py
@@ -5,27 +5,30 @@ import numpy as np
 from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 
+
 def mask(fastq_file):
-    "Takes a fastq file and returns a pandas dataframe with the values, basecalls with a phredscore under 30 are masked with 'N'"
-    df = pd.DataFrame(pd.read_table(fastq_file, header=None).values.reshape(-1, 4), columns = ['read_id', 'seq','+', 'qual']).drop(columns="+") # reads a fastq file into a dataframe where each entry is indexed at the column as read_id,seq and qual
-    df_qual = df["qual"].str.split(pat="",expand=True) # splitting the quality into each individual charecter to assess their value
-    df_seq = df["seq"].str.split(pat="",expand=True).fillna("") # splitting the sequence into each individual charecters, filling the gaps of shorter sequences with empty strings
-    phred_30_df = df_qual.replace(["!",'"',"#","$","%","&","'","(",")","*","+",",","-",".","/","0","1","2","3","4","5","6","7","8","9",":",";","<","=",">"],value = "N") #
-    masked_sequence_df =df_seq.iloc[:,7:].mask(phred_30_df == "N","N") # maks base calls with a phred score of 30 or lower, disregards the first 6 basecalls (the barcode)
-    frames = [df_seq.iloc[:,:7],masked_sequence_df] #joins the barcode and the masked sequence
-    x = pd.concat(frames,axis=1) #joins the barcode and the masked sequence
-    df["seq"] = x.apply((lambda x: "".join(x)),axis=1) # why are random N's added to the shorter sequences?
+    """Takes a fastq file and returns a pandas dataframe with the values, basecalls with a phredscore under 30 are masked with 'N' """
+    df = pd.DataFrame(pd.read_table(fastq_file, header=None).values.reshape(-1, 4), columns=['read_id', 'seq', '+', 'qual']).drop(columns="+")  # reads a fastq file into a dataframe where each entry is indexed at the column as read_id,seq and qual
+    df_qual = df["qual"].str.split(pat="", expand=True)  # splitting the quality into each individual charecter to assess their value
+    df_seq = df["seq"].str.split(pat="", expand=True).fillna("")  # splitting the sequence into each individual charecters, filling the gaps of shorter sequences with empty strings
+    phred_30_df = df_qual.replace(["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">"], value = "N") #
+    masked_sequence_df = df_seq.iloc[:, 7:].mask(phred_30_df == "N", "N")  # maks base calls with a phred score of 30 or lower, disregards the first 6 basecalls (the barcode)
+    frames = [df_seq.iloc[:, :7], masked_sequence_df]  # joins the barcode and the masked sequence
+    x = pd.concat(frames, axis=1)  # joins the barcode and the masked sequence
+    df["seq"] = x.apply((lambda x: "".join(x)), axis=1)  # why are random N's added to the shorter sequences?
     return df
 
-def fastq_pair(df1,df2):
-    "Takes two dataframes of NGS data, returns a dataframe where identical @ID's are matched, and a dataframe where all non-paired values are stored"
-    df3 = df1.merge(df2, left_on='read_id', right_on='read_id',suffixes=("1","2")) #merges values if the read id is identical. Each read ID will now have two "seq" and "qual" values
-    frames= [df1,df2]
-    df4 = pd.concat(frames).drop_duplicates("read_id",keep=False) #concats both dataframes and removes both duplicates
+
+def fastq_pair(df1, df2):
+    """Takes two dataframes of NGS data, returns a dataframe where identical @ID's are matched, and a dataframe where all non-paired values are stored"""
+    df3 = df1.merge(df2, left_on='read_id', right_on='read_id', suffixes=("1", "2")) #merges values if the read id is identical. Each read ID will now have two "seq" and "qual" values
+    frames= [df1, df2]
+    df4 = pd.concat(frames).drop_duplicates("read_id", keep=False) #concats both dataframes and removes both duplicates
     return df3
 
+
 def sort_barcodes(df):
-    "Takes a fasta file with barcodes and a fastq file with data. Returns a dataframe with barcode indexed columns"
+    """Takes a fasta file with barcodes and a fastq file with data. Returns a dataframe with barcode indexed columns"""
     series_seq = pd.Series(df["seq1"])
     series_seq2 = pd.Series(df["seq2"])
     df_sorted_by_BC1 = series_seq.str.extractall("(?P<BC1_1>^GCAAAA.+)").reset_index(drop=True)
@@ -49,16 +52,22 @@ def sort_barcodes(df):
     df_sorted_by_BC9_2 = series_seq2.str.extractall("(?P<BC9_2>^GCACAAA.+)").reset_index(drop=True)
     df_sorted_by_BC10_2 = series_seq2.str.extractall("(?P<BC10_2>^GCATAA.+)").reset_index(drop=True)
     list_of_dfs = [df_sorted_by_BC1,df_sorted_by_BC1_2,df_sorted_by_BC2,df_sorted_by_BC2_2,df_sorted_by_BC3,df_sorted_by_BC3_2, df_sorted_by_BC4,df_sorted_by_BC4_2,df_sorted_by_BC5,df_sorted_by_BC5_2,df_sorted_by_BC6,df_sorted_by_BC6_2,df_sorted_by_BC7,df_sorted_by_BC7_2,df_sorted_by_BC8,df_sorted_by_BC8_2,df_sorted_by_BC9,df_sorted_by_BC9_2,df_sorted_by_BC10,df_sorted_by_BC10_2]
+    list_of_dfs1 =[df_sorted_by_BC1,df_sorted_by_BC1_2]
+    list_of_dfs2 = [df_sorted_by_BC2,df_sorted_by_BC2_2]
     BC_df = pd.concat(list_of_dfs, axis=1).replace(r'^^[A-Z]{6}', "", regex=True) # Removes the barcode ie. the first six letters of the sequence
-    return BC_df
+    BC_df1 = pd.concat(list_of_dfs1, axis=1).replace(r'^^[A-Z]{6}', "", regex=True)
+    BC_df2 = pd.concat(list_of_dfs2, axis=1).replace(r'^^[A-Z]{6}', "", regex=True)
+    output = [BC_df1,BC_df2]
+    return (output)
+
 
 def merge(df):
-    "takes a dataframe of sequences merged by read_id, returns a dataframe where the two sequences are merged based on overlap"
+    """takes a dataframe of sequences merged by read_id, returns a dataframe where the two sequences are merged based on overlap"""
     df["BC1_2"] = df["BC1_2"].apply(lambda x: x[::-1])
     split = df["BC1_2"].str.split(pat="", expand=True)
     complemented = split.replace({"G":"C","C":"G","A":"T","T":"A"})
     df["BC1_2"] = complemented.apply((lambda x: "".join(x)), axis=1) # How to apply this on all "_2" suffixes?
-    sequence = []
+    BC1 = []
     for seq,rev in zip(df["BC1_1"],df["BC1_2"]):
         alignments = (pairwise2.align.localms(seq,rev,2,-1,-3,-3))
         consensus = []
@@ -70,11 +79,41 @@ def merge(df):
             elif s2 == "-":
                 consensus.append(s1)
             con = "".join(consensus)
+        BC1.append(con)
+    return (BC1)
+
+
+def merge2(df):
+    """takes a dataframe of sequences merged by read_id, returns a dataframe where the two sequences are merged based on overlap"""
+    x = df.iloc[:,1:2].squeeze().apply(lambda x: x[::-1])
+    y = df.iloc[:,0:1].squeeze()
+    split = x.str.split(pat="", expand=True)
+    complemented = split.replace({"G":"C","C":"G","A":"T","T":"A"})
+    df["rev"] = complemented.apply((lambda x: "".join(x)), axis=1)
+    sequence = []
+    for seq,rev in zip(y,df["rev"]):
+        alignments = (pairwise2.align.localms(seq,rev,2,-1,-3,-3))
+        consensus = []
+        for s1,s2 in zip(alignments[0][0],alignments[0][1]):
+            if s1 == s2:
+                consensus.append(s1)
+            elif s1 == "-":
+                consensus.append(s2)
+            elif s2 == "-":
+                consensus.append(s1)
+            con = "".join(consensus)
         sequence.append(con)
+    #pd.set_option('display.max_rows', 1000, 'display.max_columns', 1000, 'display.max_colwidth', 1000)
     return print(sequence)
-def df_to_gzfasta(dataframe):
-    "Takes a pd dataframe and returns a gzipped fasta file"
-    return
+
+
+def write_fasta(list_of_sequences, filename):
+    """Takes a list of sequences and a filename as a string, returns a fastafile"""
+    with open(filename, "w") as outfile:
+        for i in list_of_sequences:
+            outfile.write(">"+"\n"+i+"\n")
+    return filename
+
 
 def merge_txt_files(file1, file2, file3, name):
     "..."
@@ -86,14 +125,16 @@ def merge_txt_files(file1, file2, file3, name):
                 outfile.write(contents)
     return name
 
+
 def count_depth(file):
     "text"
     input_file = open(file)
     depth = input_file.readlines()
     return len(depth)
 
+
 def count_nucleotides(f_reads):
-    "Takes a txt file from a bowtie output with alignment position followed by the sequence seperated by a blank space, returns a dict of dicts with nucleotide position as outer key, nucleotide as inner key and number of nucleotides counted as value"
+    """Takes a txt file from a bowtie output with alignment position followed by the sequence seperated by a blank space, returns a dict of dicts with nucleotide position as outer key, nucleotide as inner key and number of nucleotides counted as value"""
     dict_of_nucleotide_positions = {}
     input_file = open(f_reads)
     lines = input_file.readlines()
@@ -114,8 +155,9 @@ def count_nucleotides(f_reads):
                 dict_of_nucleotide_positions[outerkey][innerkey] += 1
     return dict_of_nucleotide_positions
 
+
 def calculate_mutations(dict, depth):
-    "docstring"
+    """docstring"""
     dict_of_mutation_percentages = {}
     items = dict.items()
     for item in items:
@@ -129,8 +171,9 @@ def calculate_mutations(dict, depth):
             dict_of_mutation_percentages[position] = mutation_percentage
     return dict_of_mutation_percentages
 
+
 def total_mutations(dict):
-    "docstring"
+    """docstring"""
     items = dict.items()
     list_of_mutations = []
     list_of_wt = []
@@ -143,15 +186,17 @@ def total_mutations(dict):
         list_of_mutations.append(mutation)
     return sum(list_of_wt), sum(list_of_mutations), sum(list_of_mutations)/sum(list_of_wt)
 
+
 def plot_mutation_distribution(dict_of_mutation_percentages):
-    "Docstring"
+    """Docstring"""
     lists = sorted(dict_of_mutation_percentages.items())
     x, y = zip(*lists)
     plt.scatter(x, y, s=4, color="black")
     return
 
+
 def plot_substitution_base_proberbility(dict):
-    "Docstring"
+    """Docstring"""
     dict_of_wt = {"A": 0, "T": 0, "C": 0, "G": 0, "N": 0}
     A = { "T": 0, "C": 0, "G": 0}
     T = {"A": 0, "C": 0, "G": 0}
@@ -204,8 +249,9 @@ def plot_substitution_base_proberbility(dict):
     plt.tight_layout()
     return
 
+
 def plot_mutation_base_proberbility(dict):
-        "Docstring"
+        """Docstring"""
         dict_of_wt = {"A": 0, "T": 0, "C": 0, "G": 0, "N": 0}
         A = {"T": 0, "C": 0, "G": 0}
         T = {"A": 0, "C": 0, "G": 0}
