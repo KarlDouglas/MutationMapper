db37e6bc14a646e3befd1899c83693014bf7ba95
Karl Douglas Cahill <70593626+KarlDouglas@users.noreply.github.com>
2022-11-30 14:24:07 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 #SBATCH --partition=modi_short        # specify the partition to run on
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --mem=250gb                   # Job memory request
-#SBATCH --time=00:00:05               # Time limit hrs:min:sec
+#SBATCH --time=02:00:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
 srun singularity exec ~/modi_images/slurm-notebook-latest.sif ~/modi_mount/NGS_pipeline/run.sh
