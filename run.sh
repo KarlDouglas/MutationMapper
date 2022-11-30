d14f238f796494f10942d1161a2bc6b8d1cf2636
Karl Douglas Cahill <70593626+KarlDouglas@users.noreply.github.com>
2022-11-30 14:23:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 #SBATCH --mem=250gb                   # Job memory request
 #SBATCH --time=00:00:05               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
-srun singularity exec ~/modi_images/slurm-notebook-latest.sif ~/modi_mount/Seq_analysis/test.sh
+srun singularity exec ~/modi_images/slurm-notebook-latest.sif ~/modi_mount/NGS_pipeline/run.sh
