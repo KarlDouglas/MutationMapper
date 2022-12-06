b1023a00ce158cd20e350bb6719636f4cd45a15d
Karl Douglas Cahill <70593626+KarlDouglas@users.noreply.github.com>
2022-12-06 09:27:11 +0100

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,8 +1,8 @@
 import main
-mask1 = main.mask("CY001_1.fq")
-mask2 = main.mask("CY001_2.fq")
-sort = main.sort_barcodes(mask1,mask2)
+mask1 = main.mask("CY001_1.fq") # inputfile 1
+mask2 = main.mask("CY001_2.fq") # inputfile 2
+sort = main.sort_barcodes(mask1,mask2) # sorts the data by barcode
 index = 0
 for i in sort:
     index += 1
-    main.write_file(i, ("CY001BC"+str(index))+"_sorted")
+    main.write_file(i, ("CY001BC"+str(index))+"_sorted") # writes a txt file for each barcode to give to bowtie2
